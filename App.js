10fea9831501baf7547efd1b28ec7077da608afe
Eric Pan <epan@users.noreply.github.com>
2017-06-09 12:15:30 -0700

    (feat) Use flex alignment justify and stretch

--- App.js
+++ App.js
@@ -22,10 +22,15 @@ export default class App extends React.Component {
 
     return (
       // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+      }}>
+        <View style={{height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
